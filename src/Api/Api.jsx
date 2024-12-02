d62b22dff548fe0a086f5a06796f2329fd0b81e7
AbdushukurovFrontend <frontenddevelopment17@gmail.com>
2024-12-02 13:00:40 +0500

    ./

--- src/Api/Api.jsx
+++ src/Api/Api.jsx
@@ -1,3 +1,4 @@
 export const apiUrl = "https://a510c4f98367eca1.mokky.dev";
 export const sliderApi = `${apiUrl}/Slider`;
 export const aniDubApi = `${apiUrl}/aniDub`;
+export const userDate = `${apiUrl}/userDate`;
