74812fdf56c72006908e30daceacd34ac6ddac37
AbdushukurovFrontend <frontenddevelopment17@gmail.com>
2024-12-02 17:26:05 +0500

    end login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,17 +2,17 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { FaUser, FaLock, FaEye, FaEyeSlash } from "react-icons/fa"; // Iconlar
-
+import { FaUser, FaLock, FaEye, FaEyeSlash } from "react-icons/fa"; // Icons
 import "../App.css";
 import { IoIosLogOut } from "react-icons/io";
+import { FaCircleUser } from "react-icons/fa6";
 
 const Login = () => {
   const [isLogin, setIsLogin] = useState(true);
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false); // Parolni ko'rsatish / yashirish
+  const [showPassword, setShowPassword] = useState(false); // Toggle password visibility
   const navigate = useNavigate();
 
   const handleFormSubmit = () => {
@@ -37,100 +37,103 @@ const Login = () => {
   };
 
   return (
-    <div className=" bg-image-container relative">
+    <div className="bg-image-container relative h-screen">
       <button
         onClick={handleClose}
-        className="absolute top-9 right-9 text-cyan-50/100 text-center hover:text-white  hover:bg-cyan-500 hover:ps-1 rounded-lg text-[27px] z-10"
+        className="absolute top-9 right-9 text-cyan-50/100 text-center hover:text-white hover:bg-cyan-500 hover:ps-1 rounded-lg text-[27px] z-10"
       >
         <IoIosLogOut />
       </button>
-      <div className="min-h-screen flex flex-col ">
-        <div className="flex flex-col lg:flex-row h-screen">
-          {/* Chap tomondagi fon */}
-          <div className=" w-full lg:w-7/10 bg-cover bg-center">
-            {/* Tasvirga joylashgan joy bo'ladi */}
-          </div>
+      <div className="flex flex-col lg:flex-row h-full">
+        {/* Left section for background image */}
+        <div className="w-full lg:w-7/12 bg-cover bg-center bg-no-repeat" />
 
-          {/* O'ng tomondagi forma */}
-          <div className="w-[800px] lg:w-3/10 flex justify-center items-center bacgroountrans p-4">
-            <div className="form-container w-full max-w-md">
-              <h2 className="text-white text-2xl font-bold mb-4 text-center">
-                {isLogin ? "Tizimga kirish" : "Ro'yxatdan o'tish"}
-              </h2>
-              <form className="space-y-4">
-                {/* Foydalanuvchi nomi input */}
-                <div className="relative">
-                  <FaUser className="absolute left-3 top-4 text-gray-400" />
-                  <input
-                    type="text"
-                    placeholder="Foydalanuvchi nomi"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    className="input-field pl-10"
-                  />
-                </div>
+        {/* Right section for form */}
+        <div className="w-full lg:w-5/12 flex justify-center items-center p-6 bg-gradient-to-r bacgroountrans">
+          <div className="w-full max-w-md p-6 rounded-lg shadow-lg">
+            <h2 className="text-center text-2xl font-bold mb-4 text-gray-400">
+              {isLogin ? "Tizimga kirish" : "Ro'yxatdan o'tish"}
+            </h2>
+            <button></button>
+            <form className="space-y-4">
+              {/* Username Input */}
+              <div className="relative">
+                <FaUser className="absolute left-3 top-4 text-gray-400" />
+                <input
+                  type="text"
+                  placeholder="Foydalanuvchi nomi"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="input-field pl-10 w-full rounded-md border-2"
+                />
+              </div>
 
-                {/* Parol input */}
+              {/* Password Input */}
+              <div className="relative">
+                <FaLock className="absolute left-3 top-4 text-gray-400" />
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Parol"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="input-field pl-10 w-full rounded-md border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+                <span
+                  className="absolute right-3 top-4 cursor-pointer"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <FaEyeSlash className="text-gray-400" />
+                  ) : (
+                    <FaEye className="text-gray-400" />
+                  )}
+                </span>
+              </div>
+
+              {/* Repeat Password Input for Sign-Up */}
+              {!isLogin && (
                 <div className="relative">
                   <FaLock className="absolute left-3 top-4 text-gray-400" />
                   <input
                     type={showPassword ? "text" : "password"}
-                    placeholder="Parol"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    className="input-field pl-10"
+                    placeholder="Parolni takrorlang"
+                    value={repeatPassword}
+                    onChange={(e) => setRepeatPassword(e.target.value)}
+                    className="input-field pl-10 w-full rounded-md border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
                   />
-                  <span
-                    className="absolute right-3 top-4 cursor-pointer"
-                    onClick={() => setShowPassword(!showPassword)}
-                  >
-                    {showPassword ? (
-                      <FaEyeSlash className="text-gray-400" />
-                    ) : (
-                      <FaEye className="text-gray-400" />
-                    )}
-                  </span>
                 </div>
+              )}
 
-                {/* Parolni takrorlash input */}
-                {!isLogin && (
-                  <div className="relative">
-                    <FaLock className="absolute left-3 top-4 text-gray-400" />
-                    <input
-                      type={showPassword ? "text" : "password"}
-                      placeholder="Parolni takrorlang"
-                      value={repeatPassword}
-                      onChange={(e) => setRepeatPassword(e.target.value)}
-                      className="input-field pl-10"
-                    />
-                  </div>
-                )}
+              {/* Submit Button */}
+              <button
+                type="button"
+                onClick={handleFormSubmit}
+                className="btn-primary w-full py-3 rounded-md text-white bg-blue-600 hover:bg-blue-700"
+              >
+                {isLogin ? "Kirish" : "Ro'yxatdan o'tish"}
+              </button>
+            </form>
 
-                <button
-                  type="button"
-                  onClick={handleFormSubmit}
-                  className="btn-primary"
-                >
-                  {isLogin ? "Kirish" : "Ro'yxatdan o'tish"}
-                </button>
-              </form>
-              <div className="text-center mt-4">
-                <button
-                  onClick={() => setIsLogin(!isLogin)}
-                  className="text-blue-500 hover:underline"
-                >
-                  {isLogin ? "Ro'yxatdan o'tish" : "Tizimga kirish"}
-                </button>
-              </div>
-              <p className="text-gray-500 text-xs mt-4 text-center">
-                Kirish orqali siz Foydalanuvchi shartlari va Maxfiylik siyosati
-                bilan rozi bo'lasiz
-              </p>
+            {/* Switch between Login and Sign-Up */}
+            <div className="text-center mt-4 flex justify-between">
+              <span>
+                {isLogin ? " Hisobinggiz yo'qmi" : "hisobingiz bormi "}
+              </span>
+              <button
+                onClick={() => setIsLogin(!isLogin)}
+                className="text-blue-500 hover:underline"
+              >
+                {isLogin ? "Ro'yxatdan o'tish" : "Tizimga kirish"}
+              </button>
             </div>
+            <p className="text-gray-500 text-xs mt-4 text-center">
+              Kirish orqali siz Foydalanuvchi shartlari va Maxfiylik siyosati
+              bilan rozi bo'lasiz
+            </p>
           </div>
         </div>
-        <ToastContainer />
       </div>
+      <ToastContainer />
     </div>
   );
 };
